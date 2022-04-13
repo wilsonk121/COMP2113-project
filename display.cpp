8f07db65fa34e6082cb4047993da46830d6e0609
wilsonk121 <99470223+wilsonk121@users.noreply.github.com>
2022-04-13 21:31:26 +0800

    Update display.cpp

--- display.cpp
+++ display.cpp
@@ -6,15 +6,15 @@ int main(){
     int size=3;
     for(int i=0;i<size;i++){
         for(int j=0;j<size;j++){
-            cout<<"╔"<<"═"<<"═"<<"═"<<"╗";
+            cout<<"╔"<<"═"<<"╬"<<"═"<<"╗";
         }
         cout<<endl;
         for(int j=0;j<size;j++){
-            cout<<"║"<<" "  <<" "<<" "<<"║";
+            cout<<"╬"<<" "  <<" "<<" "<<"╬";
         }
         cout<<endl;
         for(int j=0;j<size;j++){
-            cout<<"╚"<<"═"<<"═"<<"═"<<"╝";
+            cout<<"╚"<<"═"<<"╬"<<"═"<<"╝";
         }
         cout<<endl;
 
