ec9de972e872c04297e8e563446a9740d92b239d
Selina <99546713+h3580051@users.noreply.github.com>
2022-05-01 02:10:03 +0800

    Update other.h

--- other.h
+++ other.h
@@ -1,3 +1,8 @@
+#ifndef OTHER_H
+#define OTHER_H
 
 void generate_map(char ** &grid, int &grid_size_row, int &grid_size_col);
 void random_start_and_finish_points(int &start_row, int &start_col, int &finish_row, int &finish_col, int grid_size_row, int grid_size_col);
+void move_action(char **&grid, int &steps_walked, int &current_row, int &current_col, int grid_size_row, int grid_size_col);
+
+#endif
