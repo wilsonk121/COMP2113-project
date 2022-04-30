9f854eb90227e953e37dd355dde02be35a0a6916
Selina <99546713+h3580051@users.noreply.github.com>
2022-05-01 02:04:11 +0800

    Create other.h

---
+++ other.h
@@ -0,0 +1,3 @@
+
+void generate_map(char ** &grid, int &grid_size_row, int &grid_size_col);
+void random_start_and_finish_points(int &start_row, int &start_col, int &finish_row, int &finish_col, int grid_size_row, int grid_size_col);
