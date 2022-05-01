9f347a61afdb570b4286bb53eb98932642b0db1d
Selina <99546713+h3580051@users.noreply.github.com>
2022-05-01 15:25:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -105,8 +105,61 @@ void random_start_and_finish_points(int &start_row, int &start_col, int &finish_
         finish_row = rand() % grid_size_row + 1;
     	finish_col = rand() % grid_size_col + 1;
     }
-    cout << "Starting position: Row " << start_row << " Column " << start_col << endl;
+    cout << "\nStarting position: Row " << start_row << " Column " << start_col << endl;
     cout << "Finishing position: Row " << finish_row  << " Column " << finish_col << endl;
+}
+
+//to check if the inputted direction and steps are valid
+bool move_action_valid (char direction, int number_of_step, int current_row, int current_col, int grid_size_row, int grid_size_col){
+    if (number_of_step <= 0){
+        cout << "Invalid number of steps. Please input again." << endl;
+        return false;
+    }
+    else if (direction =='U' && number_of_step < current_row)
+        return true;
+    else if (direction == 'D' && number_of_step <= (grid_size_row - current_row)){
+        return true;
+    }
+    else if (direction == 'L' && number_of_step < current_col){
+        return true;
+    }
+    else if (direction == 'R' && number_of_step <= (grid_size_col - current_col)){
+        return true;
+    }
+    else {
+        cout << "Walking outside of the map boundary. please input again" << endl;
+        return false;
+    }
+}
+
+// to update the grid with the latest walking path
+void move_action_path(char ** &grid, char direction, int number_of_step, int &next_row, int &next_col, int current_row, int current_col){
+    switch (direction) {
+        case 'U':
+            for (int i = 0; i < number_of_step; ++i){
+                grid[current_row - i][current_col] = 'U';
+            }
+            next_row -= number_of_step;
+            break;
+        case 'D' :
+            for (int i = 0; i < number_of_step; ++i){
+                grid[current_row + i][current_col] = 'D';
+            }
+            next_row += number_of_step;
+            break;
+        case 'L':
+            for (int i = 0; i < number_of_step; ++i){
+                grid[current_row][current_col - i] = 'L';
+            }
+            next_col -= number_of_step;
+            break;
+        case 'R':
+            for (int i = 0; i < number_of_step; ++i){
+                grid[current_row][current_col + i] = 'R';
+            }
+            next_col += number_of_step;
+            break;
+    }
 }
 
 //to move the player's direction and update the map with walking record and current position
@@ -116,35 +169,19 @@ void move_action(char ** &grid, int &steps_walked, int &current_row, int &curren
     char direction;
     int number_of_step;
     
+    //to get valid players' input of direction and number of step
+    do {
     cout << "\nWhich direction you want to walk towards?" << endl;
-    cout << "Enter 'U' for upwards, 'D' for downwards, 'L' for leftwarda, 'R' for rightwards" << endl;
+    cout << "Enter 'U' for upwards, 'D' for downwards, 'L' for leftwarda, 'R' for rightwards"<< endl;
     cin >> direction;
     cout << "How many steps do you want to take? ";
     cin >> number_of_step;
+    }
+    while (move_action_valid(direction,number_of_step, current_row, current_col, grid_size_row, grid_size_col) == false);
     
+    //to update the walking path
     int next_row = current_row, next_col = current_col;
-    
-    if (number_of_step <= 0)
-        cout << "Invalid number of steps. Please input again." << endl;
-    else if (direction =='U' && number_of_step < current_row){
-        grid[current_row][current_col] = 'U';
-        next_row -= number_of_step;
-    }
-    else if (direction == 'D' && number_of_step <= (grid_size_row - current_row)){
-        grid[current_row][current_col] = 'D';
-        next_row += number_of_step;
-    }
-    else if (direction == 'L' && number_of_step < current_col){
-        grid[current_row][current_col] = 'L';
-        next_col -= number_of_step;
-    }
-            
-    else if (direction == 'R' && number_of_step <= (grid_size_col - current_col)){
-        grid[current_row][current_col] = 'R';
-        next_col += number_of_step;
-    }
-    else 
-        cout << "Walking outside of the map boundary. please input again" << endl;
+    move_action_path(grid, direction, number_of_step, next_row, next_col, current_row, current_col);
 	
     //to update the total steps walked and the current position
     if (current_row != next_row || current_col != next_col){
@@ -155,8 +192,6 @@ void move_action(char ** &grid, int &steps_walked, int &current_row, int &curren
     }
         
 }
-
-
 
 int main(){
     
