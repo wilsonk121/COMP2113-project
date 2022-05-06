f461e8fab86de2c5ee1e60af3d299fa89e38ec6e
wilsonk121 <99470223+wilsonk121@users.noreply.github.com>
2022-05-06 23:01:36 +0800

    Update

--- display.cpp
+++ display.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include "other.h"
 using namespace std;
-int main(){
-    int size=3;
-    for(int i=0;i<size;i++){
-        for(int j=0;j<size;j++){
-            cout<<"╔"<<"═"<<"╬"<<"═"<<"╗";
-        }
-        cout<<endl;
-        for(int j=0;j<size;j++){
-            cout<<"╬"<<" "  <<" "<<" "<<"╬";
-        }
-        cout<<endl;
-        for(int j=0;j<size;j++){
-            cout<<"╚"<<"═"<<"╬"<<"═"<<"╝";
-        }
-        cout<<endl;
+void display(char ** grid, int grid_size_row, int grid_size_col, int current_row, int current_col, int finish_row, int finish_col, 
+	     int distance_start_finish, int steps_walked){   
+	cout << endl;
+	for(int i=1;i<=grid_size_row;i++){
+            for(int j=1;j<=grid_size_col;j++){
+                cout<<"-"<<"-"<<"-"<<"-"<<"-";
+            }
+            cout<<endl;
+            for(int j=1;j<=grid_size_col;j++){
+                    switch(grid[i][j]){
+                            case 'L':
+                            cout<<"|"<<" < "<<"|";
+                            break;
+                            case 'R':
+                            cout<<"|"<<" > "<<"|";
+                            break;
+                            case 'U':
+                            cout<<"|"<<" ^ "<<"|";
+                            break;
+                            case 'D':
+                            cout<<"|"<<" ! "<<"|";
+                            break;
+                            case 'C':
+                            cout<<"|"<<" x "<<"|";
+                            break;
+                            default:
+                            cout<<"|"<<"   "<<"|";
+                            break;
+                }
+            }
 
-    }
+            
+            cout<<endl;
+            for(int j=1;j<=grid_size_col;j++){
+                cout<<"-"<<"-"<<"-"<<"-"<<"-";
+            }
+            cout<<endl;
+        }
+    //to provide statistics and hint to the player 
+    distance_current_finish = abs(current_row - finish_row) + abs(current_col - finish_col);
+    cout << "Shortest steps from starting point to exit point: " << distance_start_finish << endl;
+    cout << "Shortest steps from current point to exit point: " << distance_current_finish << endl;
+    cout << "Total steps walked from the starting point: " << steps_walked << endl;
 }
- 
