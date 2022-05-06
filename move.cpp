7d68f0c369f91e15319c552bd971191e32998523
wilsonk121 <99470223+wilsonk121@users.noreply.github.com>
2022-05-06 23:13:13 +0800

    Update

--- move.cpp
+++ move.cpp
@@ -16,41 +16,41 @@ void load_map(char ** &grid, int &grid_size_row, int &grid_size_col, int & curre
 void output_record(char ** grid, int grid_size_row, int grid_size_col, int finish_row, int finish_col, bool &savegame){
     string name;
     ofstream fout;
-    cout<<"Input the file name: ";
-    cin>>name;
+    cout << "Input the file name: ";
+    cin >> name;
     fout.open(name);
     if(fout.fail()){
         cout<<"Error in file opening"<<endl;
         exit(1);
     }
-    fout<<grid_size_row<<endl;
-    fout<<grid_size_col<<endl;
-    fout<<finish_row<<endl;
-    fout<<finish_col<<endl;
+    fout << grid_size_row<<endl;
+    fout << grid_size_col<<endl;
+    fout << finish_row<<endl;
+    fout << finish_col<<endl;
 
-    for(int i=1;i<=grid_size_row;i++){
-        for(int j=1;j<=grid_size_col;j++){
-            if(grid[i][j]=='C'){
-                fout<<i<<endl;
-                fout<<j<<endl;
+    for(int i = 1; i <= grid_size_row; i++){
+        for(int j = 1; j <= grid_size_col; j++){
+            if(grid[i][j] == 'C'){
+                fout << i << endl;
+                fout << j << endl;
 
             }
         }
     }
-    for(int i=1;i<=grid_size_row;i++){
-        for(int j=1;j<=grid_size_col;j++){
-            if(grid[i][j]=='U'||grid[i][j]=='D'||grid[i][j]=='L'||grid[i][j]=='R'||grid[i][j]=='C'){
-                fout<<grid[i][j]<<endl;
-                fout<<i<<endl;
-                fout<<j<<endl;
+    for(int i = 1; i <= grid_size_row; i++){
+        for(int j = 1; j <= grid_size_col; j++){
+            if(grid[i][j] == 'U'|| grid[i][j] == 'D'|| grid[i][j] == 'L'|| grid[i][j] == 'R'||grid[i][j] == 'C'){
+                fout << grid[i][j] << endl;
+                fout << i << endl;
+                fout << j << endl;
             }
 
         }
     }
 
     fout.close();
-    cout<<"game saved in "<<name<<endl;
-    savegame=1;
+    cout << "game saved in " << name <<endl;
+    savegame = 1;
 
 }
 
