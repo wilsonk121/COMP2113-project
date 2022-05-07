a0cf3725c95895b94832c9d616e791ce8fe79e16
wilsonk121 <99470223+wilsonk121@users.noreply.github.com>
2022-05-07 12:02:40 +0800

    Update display.cpp

--- display.cpp
+++ display.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
 #include "other.h"
 
-using namespace std
+using namespace std;
 	
 //to display the essential information for the game such as map, user location, distance from the end point
 void display(char ** grid, int grid_size_row, int grid_size_col, int current_row, int current_col, int finish_row, int finish_col, int distance_start_finish, int steps_walked){   
